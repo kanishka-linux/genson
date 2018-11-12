d7cd3469e1977f533fa1d18f651659f2922c927b
kanishka-linux <kanishka.linux@gmail.com>
2018-11-12 23:11:16 +0530

    add separate module for cases when type not present

---
+++ lib/jake/notype.ex
@@ -0,0 +1,36 @@
+defmodule Jake.Notype do
+  @prop %{
+    "minLength" => "string",
+    "maxLength" => "string",
+    "pattern" => "string",
+    "multipleOf" => "number",
+    "minimum" => "number",
+    "maximum" => "number",
+    "exclusiveMinimum" => "number",
+    "exclusiveMaximum" => "number",
+    "items" => "array",
+    "additionalItems" => "array",
+    "minItems" => "array",
+    "maxItems" => "array",
+    "uniqueItems" => "array",
+    "properties" => "object",
+    "additionalProperties" => "object",
+    "required" => "object",
+    "minProperties" => "object",
+    "maxProperties" => "object"
+  }
+
+  def gen_notype(map, type) do
+    nmap = for {k, v} <- map, into: %{}, do: {k, v}
+    nlist = for {k, v} <- map, into: [], do: @prop[k]
+
+    types =
+      Enum.reduce(nlist, nil, fn
+        x, acc when not is_nil(x) -> x
+        x, acc when is_nil(x) -> acc
+      end)
+
+    nmap = if not is_nil(types), do: Map.put(nmap, "type", types), else: nmap
+    if nmap["type"], do: Jake.gen_init(nmap)
+  end
+end
