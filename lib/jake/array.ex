b3803e9020694411f2fab0cc50730694364a4520
kanishka-linux <kanishka.linux@gmail.com>
2018-11-18 19:41:12 +0530

    separate tuple and list generation methods

--- lib/jake/array.ex
+++ lib/jake/array.ex
@@ -12,28 +12,50 @@ defmodule Jake.Array do
 
   @max_items 1000
 
-  def gen_array(map, type), do: arraytype(map, map["enum"], map["items"])
+  def gen_array(%{"items" => items} = map, _type) do
+    case items do
+      item when is_map(item) ->
+        gen_list(map, item)
+
+      item when is_list(item) ->
+        gen_tuple(map, item)
+
+      _ ->
+        raise "Invalid items in array"
+    end
+  end
+
+  def gen_array(map, _type), do: arraytype(map, map["enum"], map["items"])
 
   def arraytype(map, enum, items) when enum != nil, do: Jake.gen_enum(enum, "array")
 
-  def arraytype(map, enum, items) when is_list(items) do
+  def arraytype(map, enum, items) when is_nil(items) and is_nil(enum) do
+    item = get_one_of()
+    {min, max} = get_min_max(map)
+    decide_min_max(map, item, min, max)
+  end
+
+  def gen_tuple(map, items) do
     list = for n <- items, is_map(n), do: Jake.gen_init(n)
 
     {min, max} = get_min_max(map)
 
     case map["additionalItems"] do
-      x when (is_boolean(x) and x) or is_nil(x) ->
-        add_additional_items(list, true, max, min)
-
       x when is_map(x) ->
-        add_additional_items(list, x, max, min)
+        add_additional_items(list, Jake.gen_init(x), max, min)
+
+      x when (is_boolean(x) and x) or is_nil(x) ->
+        add_additional_items(list, get_one_of(), max, min)
+
+      x when is_boolean(x) and not x and length(list) in min..max ->
+        StreamData.fixed_list(list)
 
       _ ->
-        add_additional_items(list, false, max, min)
+        raise "Invalid items or length of list exceeds specified bounds"
     end
   end
 
-  def arraytype(map, enum, items) when is_map(items) do
+  def gen_list(map, items) do
     {min, max} = get_min_max(map)
     item = Jake.gen_init(items)
     decide_min_max(map, item, min, max)
@@ -43,12 +65,6 @@ defmodule Jake.Array do
     min = Map.get(map, "minItems", @min_items)
     max = Map.get(map, "maxItems", @max_items)
     {min, max}
-  end
-
-  def arraytype(map, enum, items) when is_nil(items) and is_nil(enum) do
-    item = get_one_of()
-    {min, max} = get_min_max(map)
-    decide_min_max(map, item, min, max)
   end
 
   def decide_min_max(map, item, min, max)
@@ -64,21 +80,7 @@ defmodule Jake.Array do
     for(n <- @type_list, is_map(n), do: Jake.gen_init(n)) |> StreamData.one_of()
   end
 
-  def add_additional_items(list, bool, max, min) when is_boolean(bool) and bool do
-    generate_list(list, get_one_of(), max, min)
-  end
-
-  def add_additional_items(list, bool, max, min) when is_boolean(bool) and not bool do
-    if length(list) in min..max do
-      StreamData.fixed_list(list)
-    end
-  end
-
-  def add_additional_items(list, map, max, min) when is_map(map) do
-    generate_list(list, Jake.gen_init(map), max, min)
-  end
-
-  def generate_list(olist, additional, max, min) do
+  def add_additional_items(olist, additional, max, min) do
     StreamData.bind(StreamData.fixed_list(olist), fn list ->
       StreamData.bind_filter(
         StreamData.list_of(additional),
@@ -91,7 +93,7 @@ defmodule Jake.Array do
           when length(list) in min..max ->
             {:cont, StreamData.constant(list)}
 
-          nlist when true ->
+          _ ->
             :skip
         end
       )
